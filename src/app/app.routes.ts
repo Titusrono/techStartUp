752ff3ce39fc59f64f0d39d809194fa51915e14b
Titusrono <tituskrono04@gmail.com>
2024-10-07 19:01:43 +0300

    components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {'path':'', 'title':'Home', component: HomeComponent},
+    {'path': 'service', 'title':'Services', component: ServicesComponent},
+    {'path':'contact', 'title':'Contact', component: ContactComponent},
+    {'path':'about', 'title':'About', component: AboutComponent},
+    {'path':'blog', 'title':'Blog', component: BlogComponent},
+    {'path':'**', 'title':'404', component: PagenotfoundComponent}
+];
